cfa39f355048c1260f16d45080944e8cad7edbc8
disVill <55872725+disVill@users.noreply.github.com>
2020-04-06 00:08:06 +0900

    refacter

--- cogs/music.py
+++ cogs/music.py
@@ -123,7 +123,7 @@ class music(commands.Cog):
 
             await self.play_next.wait()
 
-    # 曲を再生するコマンド
+    # YouTube-URLまたは曲名から曲を再生
     @commands.command(enabled=is_enabled)
     async def play(self, ctx, *, url: str):
         if (self.voice is None) or (not self.voice.is_connected()):
